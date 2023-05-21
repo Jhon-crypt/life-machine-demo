675744acf0426823e83e6b4c674f02ee231089c6
Jhon-crypt <johnoladele690@gmail.com>
2023-05-21 18:58:08 +0100

    Made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import Hero from "./components/hero/hero"
-//import Build from "./components/build"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartBuild from "./components/build/startBuild";
 
 function App() {
@@ -8,13 +5,8 @@ function App() {
   return (
     <>
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="home" element={<Hero />} />
-          <Route path="startBuild" element={<StartBuild />} />
+      <StartBuild />
           
-        </Routes>
-      </BrowserRouter>
 
     </>
   )
